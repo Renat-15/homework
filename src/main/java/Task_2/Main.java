3881a335a03c8766c08eaecb2c1eba54962c04dc
Renat Khamatnurov <renat200115@mail.ru>
2024-10-27 20:28:25 +0300

    Task1_done_

--- src/main/java/Task_2/Main.java
+++ src/main/java/Task_2/Main.java
@@ -10,8 +10,8 @@ public class Main {
         System.out.println("Цвет фона: " + circle.getColor());
         System.out.println("Цвет границ: " + circle.getBorderColor());
         System.out.println();
-        Rectangle rectangle = new Rectangle("White", "Pink", 5, 5);
 
+        Rectangle rectangle = new Rectangle("White", "Pink", 5, 5);
         System.out.println("Прямоугольник: ");
         System.out.println("Площадь: " + rectangle.calculateArea());
         System.out.println("Периметр: " + rectangle.calculatePerimeter());
@@ -20,7 +20,6 @@ public class Main {
         System.out.println();
 
         Triangle triangle = new Triangle("Black", "Green", 10, 10, 10);
-
         System.out.println("Треугольник: ");
         System.out.println("Площадь: " + triangle.calculateArea());
         System.out.println("Периметр: " + triangle.calculatePerimeter());
