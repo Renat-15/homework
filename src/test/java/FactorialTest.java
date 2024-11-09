a53b4d5c0624819493ac1e997d67f99740283262
Renat Khamatnurov <renat200115@mail.ru>
2024-11-10 00:09:52 +0300

    donegit add .

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test;
 public class FactorialTest {
 
     @Test
-    public void correctFactorial(){ // Тест для успешного подсчёта факториала
+    public void correctFactorialZero(){ // Тест для успешного подсчёта факториала нуля
         Factorial factorial = new Factorial();
         int number = factorial.calculateFactorial(0);
         Assertions.assertEquals(1, number);
+    }
+
+    @Test
+    public void correctFactorialDefault(){ // Тест для успешного подсчёта факториала обычного числа
+        Factorial factorial = new Factorial();
+        int number = factorial.calculateFactorial(5);
+        Assertions.assertEquals(120, number);
     }
     @Test
     public void incorrectFactorial(){ // Тест для неуспешной проверки
