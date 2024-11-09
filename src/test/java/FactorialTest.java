6af72cd16cbb2a4d53fd6823e3c39297e30b9149
Renat Khamatnurov <renat200115@mail.ru>
2024-11-10 00:15:12 +0300

    done

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -3,21 +3,21 @@ import org.testng.Assert;
 
 public class FactorialTest {
     @Test
-    public void correctFactorialZero(){
+    public void correctFactorialZero(){ // Тест для успешного подсчёта факториала нуля
         Factorial factorial = new Factorial();
         int number = factorial.calculateFactorial(0);
         Assert.assertEquals(1, number);
     }
 
     @Test
-    public void correctFactorialDefault(){
+    public void correctFactorialDefault(){ // Тест для успешного подсчёта факториала обычного числа
         Factorial factorial = new Factorial();
         int number = factorial.calculateFactorial(5);
         Assert.assertEquals(120, number);
     }
 
     @Test
-    public void incorrectFactorial(){
+    public void incorrectFactorial(){ // Тест для неуспешной проверки
         Factorial factorial = new Factorial();
         int number = factorial.calculateFactorial(-8);
         Assert.assertEquals(-8, number);
